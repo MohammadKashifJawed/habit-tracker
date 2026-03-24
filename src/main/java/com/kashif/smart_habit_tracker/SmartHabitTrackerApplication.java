6711fb0578083748b5d6e464e5f54304ea53ed1b
MohammadKashifJawed <kashifjawed123@gmail.com>
2026-03-24 13:08:15 +0530

    Added project to github repo

---
+++ src/main/java/com/kashif/smart_habit_tracker/SmartHabitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kashif.smart_habit_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHabitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHabitTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kashif/smart_habit_tracker/SmartHabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashif.smart_habit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
